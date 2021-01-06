3c05ca82337236725c31651412256c25094315c7
asus <alikemalagirman@gmail.com>
2021-01-06 22:27:23 +0300

    adding files for githuv pages

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./card-list.styles.css";
+import {Card} from "../card/card.component";
+
+
+export const Cardlist = props => (
+    
+    <div className="card-list"> 
+        {props.monsters.map(monster=> ( 
+            <Card key ={monster.id} monster={monster}/>
+            )
+        )
+        }
+        <img alt="monster2" src="smiley.gif"/>
+    </div>
+)
+
+
